579e9d061ff396989ff85ba0a73e9ea43074e134
kareemyasser8 <kareem.yasserr@gmail.com>
2023-04-13 09:29:53 +0200

    update profile and add options

--- src/app/profile-page/profile-page.component.ts
+++ src/app/profile-page/profile-page.component.ts
@@ -14,10 +14,12 @@ export class ProfilePageComponent implements OnInit, OnDestroy {
 
   RouteSubscription: Subscription;
   profileSubscripton: Subscription;
+  authListenerSubscription: Subscription;
 
   username: string;
   user$: Observable<any>;
   UserTweets$: Observable<Tweet[]>;
+  userIsAuthenticated: boolean;
 
 
   constructor(private authService: AuthService,
@@ -28,6 +30,7 @@ export class ProfilePageComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.RouteSubscription.unsubscribe();
     this.profileSubscripton.unsubscribe();
+    this.authListenerSubscription.unsubscribe();
   }
 
   ngOnInit(): void {
@@ -36,6 +39,9 @@ export class ProfilePageComponent implements OnInit, OnDestroy {
       this.profileSubscripton = this.authService.fetchProfile(this.username).subscribe({
         next: (result) => {
           this.user$ = of(result)
+          this.userIsAuthenticated = this.authService.getIsAuth();
+          this.authListenerSubscription = this.authService.getAuthStatusListener().subscribe()
+
           this.tweetService.getTweetsOfProfile(this.username);
           this.UserTweets$ = this.tweetService.getAllUserTweetsUpdateListener()
         },
@@ -43,10 +49,6 @@ export class ProfilePageComponent implements OnInit, OnDestroy {
       });
     });
 
-    // this.tweetService.getTweetsOfProfile(this.username);
-
-
-    // this.UserTweets$.subscribe((res)=>{console.log("result =>",res)});
 
   }
 }
